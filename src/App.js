09725b2b475f95166c6a6f9c2ef237261289b28a
Ahmad <101084690+AhmadAsif2@users.noreply.github.com>
2023-04-24 15:15:30 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  GPT3,
+  Header,
+} from './containers';
+import { Cta, Navbar } from './components';
+import Brand from './components/brand/Brand';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <GPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   );
 }
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+import './header.css';
+
+const Header = () => (
+  <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+      <h1 className="gradient__text">
+        Let&apos;s Build Something amazing with GPT-3 OpenAI
+      </h1>
+      <p>
+        Welcome to our GPT-3 website, where the power of artificial intelligence
+        is at your fingertips. Our website offers a range of cutting-edge tools
+        and services powered by GPT-3 technology, including language processing,
+        natural language generation, and machine learning.
+      </p>
+
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+
+      <div className="gpt3__header-content__people">
+        <img src={people} />
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
+    </div>
+
+    <div className="gpt3__header-image">
+      <img src={ai} />
+    </div>
+  </div>
+);
+
+export default Header;
